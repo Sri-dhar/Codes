9f60aa027e13be97add462f4b8ae8ef929140c28
sri-dhar <sridhartuli@gmail.com>
2023-10-17 15:31:02 +0530

    all correct now

--- AlgoLab/Graph/adjList.cpp
+++ AlgoLab/Graph/adjList.cpp
@@ -1,75 +1,93 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool checkEdge(const vector<pair<int, list<int>>>& adjList, int i, int j)
+bool checkEdge(const vector<pair<int, list<int>>> &adjList, int i, int j)
 {
-    if (i >= adjList.size()) {
+    if (i >= adjList.size())
+    {
         return false;
     }
 
-    const list<int>& l = adjList[i].second;
+    const list<int> &l = adjList[i].second;
     auto it = find(l.begin(), l.end(), j);
 
-    if (it != l.end()) {
+    if (it != l.end())
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
-void addNode(vector<pair<int, list<int>>>& adjList){
+void addNode(vector<pair<int, list<int>>> &adjList)
+{
     int n = adjList.size();
     adjList.push_back(make_pair(n, list<int>()));
-    cout<<"Enter the connected nodes of "<<n<<endl;
-    cout<<"Enter -1 to stop : "<<endl;
-    while(1)
+    cout << "Enter the connected nodes of " << n << endl;
+    cout << "Enter -1 to stop : " << endl;
+    while (1)
     {
-        int temp ;
-        cin>>temp;
-        if(temp != -1){
+        int temp;
+        cin >> temp;
+        if (temp != -1)
+        {
             adjList[n].second.push_back(temp);
             adjList[temp].second.push_back(n);
         }
-        else break;
+        else
+            break;
     }
 }
 
-void listAllEdge(const vector<pair<int, list<int>>>& adjList){
-    for(int i=0; i<adjList.size(); i++){
-        cout<<"Edges connected to node "<<i<<" : ";
-        for(auto j: adjList[i].second){
-            cout<<i<<"-"<<j<<" ";
+void listAllEdge(const vector<pair<int, list<int>>> &adjList)
+{
+    for (int i = 0; i < adjList.size(); i++)
+    {
+        cout << "Edges connected to node " << i << " : ";
+        for (auto j : adjList[i].second)
+        {
+            cout << i << "-" << j << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-void printAdjList(const vector<pair<int, list<int>>>& adjList){
-    for(int i=0; i<adjList.size(); i++){
-        cout<<"Node "<<i<<" : ";
-        for(auto j: adjList[i].second){
-            cout<<j<<" ";
+void printAdjList(const vector<pair<int, list<int>>> &adjList)
+{
+    for (int i = 0; i < adjList.size(); i++)
+    {
+        cout << "Node " << i << " : ";
+        for (auto j : adjList[i].second)
+        {
+            cout << j << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-void insertEdge(vector<pair<int, list<int>>>& adjList, int i, int j){
+void insertEdge(vector<pair<int, list<int>>> &adjList, int i, int j)
+{
     adjList[i].second.push_back(j);
     adjList[j].second.push_back(i);
 }
 
-void deleteEdge(vector<pair<int, list<int>>>& adjList, int i, int j){
+void deleteEdge(vector<pair<int, list<int>>> &adjList, int i, int j)
+{
     adjList[i].second.remove(j);
     adjList[j].second.remove(i);
 }
 
-vector<vector<int>> convertListToMatrix(const vector<pair<int, list<int>>>& adjList){
+vector<vector<int>> convertListToMatrix(const vector<pair<int, list<int>>> &adjList)
+{
     int n = adjList.size();
     vector<vector<int>> adjMatrix(n, vector<int>(n, 0));
 
-    for(int i=0; i<n; i++){
-        for(auto j: adjList[i].second){
+    for (int i = 0; i < n; i++)
+    {
+        for (auto j : adjList[i].second)
+        {
             adjMatrix[i][j] = 1;
         }
     }
@@ -77,7 +95,8 @@ vector<vector<int>> convertListToMatrix(const vector<pair<int, list<int>>>& adjL
     return adjMatrix;
 }
 
-void BFS(const vector<pair<int, list<int>>>& adjList, int startNode) {
+void BFS(const vector<pair<int, list<int>>> &adjList, int startNode)
+{
     int n = adjList.size();
     vector<bool> visited(n, false);
     queue<int> q;
@@ -87,23 +106,28 @@ void BFS(const vector<pair<int, list<int>>>& adjList, int startNode) {
     q.push(startNode);
     visited[startNode] = true;
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int current = q.front();
         cout << current << " ";
         q.pop();
 
-        for (int neighbor : adjList[current].second) {
-            if (!visited[neighbor]) {
+        for (int neighbor : adjList[current].second)
+        {
+            if (!visited[neighbor])
+            {
                 q.push(neighbor);
                 visited[neighbor] = true;
             }
         }
     }
 
-    cout << "\n" << endl;
+    cout << "\n"
+         << endl;
 }
 
-void DFS(const vector<pair<int, list<int>>>& adjList, int startNode) {
+void DFS(const vector<pair<int, list<int>>> &adjList, int startNode)
+{
     int n = adjList.size();
     vector<bool> visited(n, false);
 
@@ -111,23 +135,28 @@ void DFS(const vector<pair<int, list<int>>>& adjList, int startNode) {
     stack<int> s;
     s.push(startNode);
 
-    while (!s.empty()) {
+    while (!s.empty())
+    {
         int current = s.top();
         s.pop();
 
-        if (!visited[current]) {
+        if (!visited[current])
+        {
             cout << current << " ";
             visited[current] = true;
 
-            for (int neighbor : adjList[current].second) {
-                if (!visited[neighbor]) {
+            for (int neighbor : adjList[current].second)
+            {
+                if (!visited[neighbor])
+                {
                     s.push(neighbor);
                 }
             }
         }
     }
 
-    cout << "\n" << endl;
+    cout << "\n"
+         << endl;
 }
 
 void checkIfConnected(vector<pair<int, list<int>>> &adjList)
@@ -140,28 +169,35 @@ void checkIfConnected(vector<pair<int, list<int>>> &adjList)
     q.push(startNode);
     visited[startNode] = true;
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int current = q.front();
         cout << current << " ";
         q.pop();
 
-        for (int neighbor : adjList[current].second) {
-            if (!visited[neighbor]) {
+        for (int neighbor : adjList[current].second)
+        {
+            if (!visited[neighbor])
+            {
                 q.push(neighbor);
                 visited[neighbor] = true;
             }
         }
     }
 
-    cout << "\n" << endl;
-    for(auto i: visited) if(i == false) {
-        cout<<"The graph is not connected "<<endl;
-        return;
-    }
-    cout<<"The graph is connected ";
+    cout << "\n"
+         << endl;
+    for (auto i : visited)
+        if (i == false)
+        {
+            cout << "The graph is not connected " << endl;
+            return;
+        }
+    cout << "The graph is connected ";
 }
 
-void AdvancedDFS(const vector<pair<int, list<int>>>& adjList) {
+void AdvancedDFS(const vector<pair<int, list<int>>> &adjList)
+{
     cout << "Enter the start node of DFS: ";
     int startNode;
     cin >> startNode;
@@ -175,10 +211,12 @@ void AdvancedDFS(const vector<pair<int, list<int>>>& adjList) {
 
     s.push(startNode);
 
-    while (!s.empty()) {
+    while (!s.empty())
+    {
         int u = s.top();
 
-        if (!visited[u]) {
+        if (!visited[u])
+        {
             visited[u] = true;
             inTime[u] = ++time;
             cout << "Node " << u << " in  time : " << inTime[u] << endl;
@@ -186,15 +224,18 @@ void AdvancedDFS(const vector<pair<int, list<int>>>& adjList) {
 
         bool allNeighborsVisited = true;
 
-        for (int v : adjList[u].second) {
-            if (!visited[v]) {
+        for (int v : adjList[u].second)
+        {
+            if (!visited[v])
+            {
                 s.push(v);
                 allNeighborsVisited = false;
                 break;
             }
         }
 
-        if (allNeighborsVisited) {
+        if (allNeighborsVisited)
+        {
             s.pop();
             outTime[u] = ++time;
             cout << "Node " << u << " out time : " << outTime[u] << endl;
@@ -203,67 +244,75 @@ void AdvancedDFS(const vector<pair<int, list<int>>>& adjList) {
 }
 
 ////////////////////////////BIPARTITE/////////////////////////////////////
-bool isBipartite(const vector<pair<int,list<int>>>& adjList) 
+bool isBipartite(const vector<pair<int, list<int>>> &adjList)
 {
     int n = adjList.size();
-    vector<int> color(n, -1);  
-    for (int startNode = 0; startNode < n; startNode++) 
+    vector<int> color(n, -1);
+    for (int startNode = 0; startNode < n; startNode++)
     {
 
-        if (color[startNode] != -1) continue;
-        color[startNode] = 0; 
+        if (color[startNode] != -1)
+            continue;
+        color[startNode] = 0;
 
         queue<int> q;
         q.push(startNode);
 
-        while (!q.empty()) 
+        while (!q.empty())
         {
             int current = q.front();
             q.pop();
 
-            for (int neighbor : adjList[current].second) 
+            for (int neighbor : adjList[current].second)
             {
-                if (color[neighbor] == -1) 
+                if (color[neighbor] == -1)
                 {
-                    color[neighbor] = 1 - color[current];  
+                    color[neighbor] = 1 - color[current];
                     q.push(neighbor);
-                } 
-                else if (color[neighbor] == color[current]) return false; 
+                }
+                else if (color[neighbor] == color[current])
+                    return false;
             }
         }
     }
-    return true; 
+    return true;
 }
 ///////////////////////////////////ENDS HERE/////////////////////////////////
 
-int main() {
+int main()
+{
     cout << "Enter the number of nodes: ";
     int n;
     cin >> n;
-    cout << "\nThe code uses zero-based indexing\n" << endl;
+    cout << "\nThe code uses zero-based indexing\n"
+         << endl;
 
     vector<pair<int, list<int>>> adjList(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         adjList[i] = make_pair(i, list<int>());
     }
 
-    for(int i=0; i<adjList.size(); i++)
+    for (int i = 0; i < adjList.size(); i++)
     {
-        cout<<"Enter the connected nodes of "<<i<<endl;
-        cout<<"Enter -1 to stop : "<<endl;
-        while(1)
+        cout << "Enter the connected nodes of " << i << endl;
+        cout << "Enter -1 to stop : " << endl;
+        while (1)
         {
-            int temp ;
-            cin>>temp;
-            if(temp != -1){
+            int temp;
+            cin >> temp;
+            if (temp != -1)
+            {
                 adjList[i].second.push_back(temp);
             }
-            else break;
+            else
+                break;
         }
     }
 
     int choice;
-    do {
+    do
+    {
         cout << "\n\nMenu:\n";
         cout << "1. List all edges\n";
         cout << "2. Insert an edge\n";
@@ -279,54 +328,63 @@ int main() {
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                listAllEdge(adjList);
-                break;
-            case 2:
-                int i, j;
-                cout << "Enter the nodes to connect: ";
-                cin >> i >> j;
-                insertEdge(adjList, i, j);
-                break;
-            case 3:
-                cout << "Enter the nodes to disconnect: ";
-                cin >> i >> j;
-                deleteEdge(adjList, i, j);
-                break;
-            case 4:
-                addNode(adjList);
-                break;
-            case 5:
-                printAdjList(adjList);
-                break;
-            case 6:
-                int startNodeBFS;
-                cout << "Enter the starting node for BFS: ";
-                cin >> startNodeBFS;
-                BFS(adjList, startNodeBFS);
-                break;
-            case 7:
-                int startNodeDFS;
-                cout << "Enter the starting node for DFS: ";
-                cin >> startNodeDFS;
-                DFS(adjList, startNodeDFS);
-                break;
-            case 8:
-                AdvancedDFS(adjList); 
-                break;
-            case 9:
-                checkIfConnected(adjList);
-                break;
-            case 10: 
-                bool a = isBipartite(adjList);
-                if(a) cout<<"The graph is bipartite\n";
-                else cout<<"The graph is not bipartite\n";
-            case 11:
-                cout << "Exiting...\n";
-                break;
-            default:
-                cout << "Invalid choice\n";
+        switch (choice)
+        {
+        case 1:
+            listAllEdge(adjList);
+            break;
+        case 2:
+            int i, j;
+            cout << "Enter the nodes to connect: ";
+            cin >> i >> j;
+            insertEdge(adjList, i, j);
+            break;
+        case 3:
+            cout << "Enter the nodes to disconnect: ";
+            cin >> i >> j;
+            deleteEdge(adjList, i, j);
+            break;
+        case 4:
+            addNode(adjList);
+            break;
+        case 5:
+            printAdjList(adjList);
+            break;
+        case 6:
+            int startNodeBFS;
+            cout << "Enter the starting node for BFS: ";
+            cin >> startNodeBFS;
+            BFS(adjList, startNodeBFS);
+            break;
+        case 7:
+            int startNodeDFS;
+            cout << "Enter the starting node for DFS: ";
+            cin >> startNodeDFS;
+            DFS(adjList, startNodeDFS);
+            break;
+        case 8:
+            AdvancedDFS(adjList);
+            break;
+        case 9:
+            checkIfConnected(adjList);
+            break;
+        case 10:
+        {
+            bool a = isBipartite(adjList);
+            if (a)
+                cout << "The graph is bipartite\n";
+            else
+                cout << "The graph is not bipartite\n";
+            break;
+        }
+        case 11:
+        {
+            cout << "Exiting...\n";
+            break;
+        }
+
+        default:
+            cout << "Invalid choice\n";
         }
     } while (choice != 11);
 

a77cc496f8ff05e4e108bb866073b1908f772716
Sridhar Tuli <sridhartuli@gmail.com>
2023-10-24 22:22:58 +0530

    deleted:    codes_cp/Codeforces/helloCode 	modified:   codes_cp/Codeforces/helloCode.cpp

--- codes_cp/Codeforces/helloCode.cpp
+++ codes_cp/Codeforces/helloCode.cpp
@@ -1,22 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IOS                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
 
 void solve()
 {
-    int m,n;
-    cin>>m>>n;
-    vector<int> arr(m);
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for (int i = 0; i < n; i++)
+        if (a[i] == 1)
+            a[i]++;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i + 1] % a[i] == 0)
+            a[i + 1]++;
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
 }
 
 int main()
 {
-    IOS 
-
-    int t;
-    t = 1;
+    IOS int t = 1;
+    cin >> t;
     while (t--)
     {
         solve();

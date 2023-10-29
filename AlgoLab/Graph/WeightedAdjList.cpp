a1e31d8a0b43cdac1efc8cad68127f27cc262ad7
Sridhar <94095513+Sri-dhar@users.noreply.github.com>
2023-10-29 09:10:57 +0530

    as

--- AlgoLab/Graph/WeightedAdjList.cpp
+++ AlgoLab/Graph/WeightedAdjList.cpp
@@ -18,62 +18,17 @@ void printList(const vector<list<pair<int, int>>> &adjList)
     cout << endl;
 }
 
-struct Edge
-{
+struct Edge {
     int u, v, weight;
 };
 
-void betterPrims(const vector<list<pair<int, int>>> &adjList)
-{   
-    cout << "Better Prims Output : "<<endl;
-    int numNodes = adjList.size();
-    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
-    vector<Edge> MSTedges;
+void findMSTprims2(const vector<list<pair<int, int>>> &adjList) {
 
-    pq.push({0, 0, 0});
-    vector<bool> isInMST(numNodes, false);
-
-    while (!pq.empty() && MSTedges.size() < numNodes - 1)
-    {
-        Edge edge = pq.top();
-        pq.pop();
-        int u = edge.u;
-        int v = edge.v;
-        int weight = edge.weight;
-
-        if (isInMST[v])
-        {
-            continue;
-        }
-
-        MSTedges.push_back({u, v, weight});
-        isInMST[v] = true;
-
-        for (auto &neighbor : adjList[v])
-        {
-            int node = neighbor.first;
-            int edgeWeight = neighbor.second;
-            if (!isInMST[node])
-            {
-                pq.push({v, node, edgeWeight});
-            }
-        }
-    }
-
-    for (const auto &edge : MSTedges)
-    {
-        cout << "Edge: " << edge.u << " - " << edge.v << " Weight: " << edge.weight << endl;
-    }
-}
-
-void findMSTprims2(const vector<list<pair<int, int>>> &adjList)
-{
+    
     vector<pair<int, pair<int, int>>> vec;
 
-    for (int i = 0; i < adjList.size(); i++)
-    {
-        for (auto &edge : adjList[i])
-        {
+    for (int i = 0; i < adjList.size(); i++) {
+        for (auto &edge : adjList[i]) {
             int node = edge.first;
             int weight = edge.second;
             vec.push_back(make_pair(weight, make_pair(i, node)));
@@ -86,8 +41,7 @@ void findMSTprims2(const vector<list<pair<int, int>>> &adjList)
     int MSTweight = 0;
     vector<Edge> MSTedges;
 
-    while (vec.size() > 0)
-    {
+    while (vec.size() > 0) {
         pair<int, pair<int, int>> edge = vec[0];
         vec.erase(vec.begin());
 
@@ -95,8 +49,7 @@ void findMSTprims2(const vector<list<pair<int, int>>> &adjList)
         int v = edge.second.second;
         int weight = edge.first;
 
-        if (!isInMST[u] || !isInMST[v])
-        {
+        if (!isInMST[u] || !isInMST[v]) {
             MSTweight += weight;
             isInMST[u] = true;
             isInMST[v] = true;
@@ -104,53 +57,86 @@ void findMSTprims2(const vector<list<pair<int, int>>> &adjList)
         }
     }
 
-    cout << "Edges of Minimum Spanning Tree: " << endl;
-    for (const auto &edge : MSTedges)
-    {
+    for (const auto &edge : MSTedges) {
         cout << "Edge: " << edge.u << " - " << edge.v << " Weight: " << edge.weight << endl;
     }
 }
 
-void findMSTprims(const vector<list<pair<int, int>>> &adjList)
-{
-    map<int, pair<int, int>> mp; // map contains the pair and their weights
-    for (int i = 0; i < adjList.size(); i++)
-    {
-        for (auto &edge : adjList[i])
-        {
-            int node = edge.first;
-            int weight = edge.second;
-            mp[weight] = make_pair(i, node);
-        }
-    }
 
-    /////////FOR PRINTING SORTED WEIGHTED EDGES//////////////
-    //
-    // for (auto &pair : mp)
-    // {
-    //     int node1 = pair.second.first;
-    //     int node2 = pair.second.second;
-    //     int weight = pair.first;
-    //     cout << "Edge: " << node1 << " - " << node2 << " Weight: " << weight << endl;
-    // }
-    priority_queue<
-        pair<int, std::pair<int, int>>,
-        vector<std::pair<int, std::pair<int, int>>>,
-        greater<std::pair<int, std::pair<int, int>>>>
-        minHeap;
+// void findMSTprims(const vector<list<pair<int, int>>> &adjList)
+// {
+//     map<int, pair<int, int>> mp; // map contains the pair and their weights
+//     for (int i = 0; i < adjList.size(); i++)
+//     {
+//         for (auto &edge : adjList[i])
+//         {
+//             int node = edge.first;
+//             int weight = edge.second;
+//             mp[weight] = make_pair(i, node);
+//         }
+//     }
 
-    for (auto &pair : mp)
-    {
-        int weight = pair.first;
-        int node1 = pair.second.first;
-        int node2 = pair.second.second;
-        minHeap.push(make_pair(weight, make_pair(node1, node2)));
-    } // all elements of the map are now in the tree
+//     /////////FOR PRINTING SORTED WEIGHTED EDGES//////////////
+//     //
+//     // for (auto &pair : mp)
+//     // {
+//     //     int node1 = pair.second.first;
+//     //     int node2 = pair.second.second;
+//     //     int weight = pair.first;
+//     //     cout << "Edge: " << node1 << " - " << node2 << " Weight: " << weight << endl;
+//     // }
+//     priority_queue<
+//         pair<int, std::pair<int, int>>,
+//         vector<std::pair<int, std::pair<int, int>>>,
+//         greater<std::pair<int, std::pair<int, int>>>>
+//         minHeap;
 
-    vector<pair<int, std::pair<int, int>>> tree;
+//     for (auto &pair : mp)
+//     {
+//         int weight = pair.first;
+//         int node1 = pair.second.first;
+//         int node2 = pair.second.second;
+//         minHeap.push(make_pair(weight, make_pair(node1, node2)));
+//     }//all elements of the map are now in the tree
 
-    tree.push_back(minHeap.top());
-}
+//     vector<pair<int, std::pair<int, int>>> tree;
+
+//     tree.push_back(minHeap.top());
+//     minHeap.pop();
+
+//     bool flagA = false, flagB = false;
+//     for(auto &i : mp)
+//     {
+//         auto node1 = 
+//     }
+
+
+//     // while (!minHeap.empty())
+//     // {
+//     //     auto top = minHeap.top();
+//     //     minHeap.pop();
+//     //     int node1 = top.second.first;
+//     //     int node2 = top.second.second;
+//     //     int weight = top.first;
+//     //     bool flag1 = false , flag2 = false;
+//     //     for (auto &edge : tree)
+//     //     {
+//     //         if (edge.second.first == node1 || edge.second.second == node1)
+//     //         {
+//     //             flag1 = true;
+//     //             break;
+//     //         }
+//     //         else if (edge.second.first == node2 || edge.second.second == node2)
+//     //         {
+//     //             flag2 = true;
+//     //             break;
+//     //         }
+//     //     }    
+//     //     tree.push_back(make_pair(weight, make_pair(node1, node2)));
+            
+
+
+// }
 
 int main()
 {
@@ -178,6 +164,7 @@ int main()
         }
     }
 
+    findMSTprims2(adjList);
     printList(adjList);
     findMSTprims2(adjList);
 

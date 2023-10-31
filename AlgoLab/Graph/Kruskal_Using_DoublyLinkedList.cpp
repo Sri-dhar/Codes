db57e3f632fe003513839dd07f0a4cb538457154
Sridhar Tuli <sridhartuli@gmail.com>
2023-10-31 14:58:17 +0530

    new file:   AlgoLab/Graph/Kruskal_Using_DoublyLinkedList.cpp

---
+++ AlgoLab/Graph/Kruskal_Using_DoublyLinkedList.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    cout << "Enter the number of nodes: ";
+    int n;
+    cin >> n;
+    cout << "\nThe code uses zero-based indexing\n";
+    cout << "Enter an edge only one time\n\n";
+
+    vector<
+}

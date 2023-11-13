81b906ed80ba45ba0649e10bccf05de0173ff447
Sridhar Tuli <sridhartuli@gmail.com>
2023-11-13 21:29:20 +0530

    sort by finish time

--- Snipp/graphAdjListWeighed.cpp
+++ Snipp/graphAdjListWeighed.cpp
@@ -3,29 +3,35 @@
 #include <list>
 #include <climits>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-void addEdge(vector<list<pair<int, int>>>& adjList, int u, int v, int w) {
+void addEdge(vector<list<pair<int, int>>> &adjList, int u, int v, int w)
+{
     adjList[u].push_back(make_pair(v, w));
     adjList[v].push_back(make_pair(u, w));
 }
 
-void BFS(vector<list<pair<int, int>>>& adjList, int V, int start) {
+void BFS(vector<list<pair<int, int>>> &adjList, int V, int start)
+{
     vector<bool> visited(V, false);
     queue<int> q;
 
     visited[start] = true;
     q.push(start);
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int u = q.front();
         q.pop();
         cout << u << " ";
 
-        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++) {
+        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+        {
             int v = i->first;
-            if (!visited[v]) {
+            if (!visited[v])
+            {
                 visited[v] = true;
                 q.push(v);
             }
@@ -34,35 +40,45 @@ void BFS(vector<list<pair<int, int>>>& adjList, int V, int start) {
 }
 
 // DFS traversal of the graph
-void DFSUtil(vector<list<pair<int, int>>>& adjList, vector<bool>& visited, int u) {
+void DFSUtil(vector<list<pair<int, int>>> &adjList, vector<bool> &visited, int u)
+{
     visited[u] = true;
     cout << u << " ";
 
-    for (auto i = adjList[u].begin(); i != adjList[u].end(); i++) {
+    for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+    {
         int v = i->first;
-        if (!visited[v]) {
+        if (!visited[v])
+        {
             DFSUtil(adjList, visited, v);
         }
     }
 }
 
-void DFS(vector<list<pair<int, int>>>& adjList, int V, int start) {
+void DFS(vector<list<pair<int, int>>> &adjList, int V, int start)
+{
     vector<bool> visited(V, false);
     DFSUtil(adjList, visited, start);
 }
 
-bool hasCycle(vector<list<pair<int, int>>>& adjList, vector<bool>& visited, int u, int parent) {
+bool hasCycle(vector<list<pair<int, int>>> &adjList, vector<bool> &visited, int u, int parent)
+{
 
-    //u is starting node
+    // u is starting node
     visited[u] = true;
 
-    for (auto i = adjList[u].begin(); i != adjList[u].end(); i++) {
+    for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+    {
         int v = i->first;
-        if (!visited[v]) {
-            if (hasCycle(adjList, visited, v, u)) {
+        if (!visited[v])
+        {
+            if (hasCycle(adjList, visited, v, u))
+            {
                 return true;
             }
-        } else if (v != parent) {
+        }
+        else if (v != parent)
+        {
             return true;
         }
     }
@@ -70,19 +86,23 @@ bool hasCycle(vector<list<pair<int, int>>>& adjList, vector<bool>& visited, int 
     return false;
 }
 
-void PrimsMinimumSpanningTree(vector<list<pair<int, int>>>& adjList, int V) {
+void PrimsMinimumSpanningTree(vector<list<pair<int, int>>> &adjList, int V)
+{
     vector<bool> visited(V, false);
     vector<int> key(V, INT_MAX);
     vector<int> parent(V, -1);
 
     key[0] = 0;
 
-    for (int i = 0; i < V - 1; i++) {
+    for (int i = 0; i < V - 1; i++)
+    {
         int u = -1;
         int min = INT_MAX;
 
-        for (int v = 0; v < V; v++) {
-            if (!visited[v] && key[v] < min) {
+        for (int v = 0; v < V; v++)
+        {
+            if (!visited[v] && key[v] < min)
+            {
                 min = key[v];
                 u = v;
             }
@@ -90,17 +110,20 @@ void PrimsMinimumSpanningTree(vector<list<pair<int, int>>>& adjList, int V) {
 
         visited[u] = true;
 
-        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++) {
+        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+        {
             int v = i->first;
             int weight = i->second;
-            if (!visited[v] && weight < key[v]) {
+            if (!visited[v] && weight < key[v])
+            {
                 key[v] = weight;
                 parent[v] = u;
             }
         }
     }
 
-    for (int i = 1; i < V; i++) {
+    for (int i = 1; i < V; i++)
+    {
         cout << parent[i] << " - " << i << endl;
     }
 }
@@ -113,8 +136,8 @@ struct DisjointSets
     DisjointSets(int n)
     {
         this->n = n;
-        parent = new int[n+1];
-        rnk = new int[n+1];
+        parent = new int[n + 1];
+        rnk = new int[n + 1];
 
         for (int i = 0; i <= n; i++)
         {
@@ -144,12 +167,15 @@ struct DisjointSets
     }
 };
 
-void KruskalMinimumSpanningTree(vector<list<pair<int, int>>>& adjList, int V) {
+void KruskalMinimumSpanningTree(vector<list<pair<int, int>>> &adjList, int V)
+{
     vector<pair<int, pair<int, int>>> edges;
     DisjointSets ds(V);
 
-    for (int u = 0; u < V; u++) {
-        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++) {
+    for (int u = 0; u < V; u++)
+    {
+        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+        {
             int v = i->first;
             int weight = i->second;
             edges.push_back(make_pair(weight, make_pair(u, v)));
@@ -158,21 +184,24 @@ void KruskalMinimumSpanningTree(vector<list<pair<int, int>>>& adjList, int V) {
 
     sort(edges.begin(), edges.end());
 
-    for (int i = 0; i < edges.size(); i++) {
+    for (int i = 0; i < edges.size(); i++)
+    {
         int u = edges[i].second.first;
         int v = edges[i].second.second;
 
         int set_u = ds.find(u);
         int set_v = ds.find(v);
 
-        if (set_u != set_v) {
+        if (set_u != set_v)
+        {
             cout << u << " - " << v << endl;
             ds.merge(set_u, set_v);
         }
     }
 }
 
-vector<int> DijkstraShortestDistanceOfAllNodesFromSource(vector<list<pair<int, int>>>& adjList, int V, int start) {
+vector<int> DijkstraShortestDistanceOfAllNodesFromSource(vector<list<pair<int, int>>> &adjList, int V, int start)
+{
     vector<int> dist(V, INT_MAX);
     vector<bool> visited(V, false);
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
@@ -180,17 +209,20 @@ vector<int> DijkstraShortestDistanceOfAllNodesFromSource(vector<list<pair<int, i
     dist[start] = 0;
     pq.push(make_pair(0, start));
 
-    while (!pq.empty()) {
+    while (!pq.empty())
+    {
         int u = pq.top().second;
         pq.pop();
 
         visited[u] = true;
 
-        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++) {
+        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+        {
             int v = i->first;
             int weight = i->second;
 
-            if (!visited[v] && dist[u] + weight < dist[v]) {
+            if (!visited[v] && dist[u] + weight < dist[v])
+            {
                 dist[v] = dist[u] + weight;
                 pq.push(make_pair(dist[v], v));
             }
@@ -200,8 +232,98 @@ vector<int> DijkstraShortestDistanceOfAllNodesFromSource(vector<list<pair<int, i
     return dist;
 }
 
+vector<int> TopologicalSort(vector<list<pair<int, int>>> &adjList, int V)
+{
+    vector<int> inDegree(V, 0);
+    vector<int> topologicalOrder;
+    queue<int> q;
 
-int main() {
+    for (int u = 0; u < V; u++)
+    {
+        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+        {
+            int v = i->first;
+            inDegree[v]++;
+        }
+    }
+
+    for (int u = 0; u < V; u++)
+    {
+        if (inDegree[u] == 0)
+        {
+            q.push(u);
+        }
+    }
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        topologicalOrder.push_back(u);
+
+        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+        {
+            int v = i->first;
+            inDegree[v]--;
+            if (inDegree[v] == 0)
+            {
+                q.push(v);
+            }
+        }
+    }
+
+    return topologicalOrder;
+}
+///////////////////////////////////////////////////////////
+
+void DFSUtil2(vector<list<pair<int, int>>> &adjList, vector<bool> &visited, vector<int> &finishTime, int &time, int u)
+{
+    visited[u] = true;
+
+    for (auto i = adjList[u].begin(); i != adjList[u].end(); i++)
+    {
+        int v = i->first;
+
+        if (!visited[v])
+        {
+            DFSUtil2(adjList, visited, finishTime, time, v);
+        }
+    }
+
+    finishTime[u] = time++;
+}
+
+vector<int> SortByFinishTimeDFS(vector<list<pair<int, int>>> &adjList, int V)
+{
+    vector<bool> visited(V, false);
+    vector<int> finishTime(V, 0);
+    vector<int> topologicalOrder;
+
+    int time = 0;
+
+    for (int u = 0; u < V; u++)
+    {
+        if (!visited[u])
+        {
+            DFSUtil2(adjList, visited, finishTime, time, u);
+        }
+    }
+
+    for (int i = 0; i < V; i++)
+    {
+        topologicalOrder.push_back(i);
+    }
+
+    sort(topologicalOrder.begin(), topologicalOrder.end(), [&](int a, int b)
+         { return finishTime[a] > finishTime[b]; });
+
+    return topologicalOrder;
+}
+
+//////////////////////////////////////////
+
+int main()
+{
     int V = 5;
     vector<list<pair<int, int>>> adjList(V);
 
@@ -216,9 +338,12 @@ int main() {
     vector<bool> visited(V, false);
     bool cycle = hasCycle(adjList, visited, 0, -1);
 
-    if (cycle) {
+    if (cycle)
+    {
         cout << "Graph contains cycle" << endl;
-    } else {
+    }
+    else
+    {
         cout << "Graph does not contain cycle" << endl;
     }
 

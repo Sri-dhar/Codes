8d4fcd887b32826d76a01833c6a7320346c7f68f
sridhartuli <sridhartuli@gmail.com>
2023-11-29 20:35:57 +0530

    Delete unnecessary code

---
+++ Python/day1.py
@@ -0,0 +1,26 @@
+print("Minimum SPanning treee")
+print("minimum spanning trie")  
+print('helo"hello:"hello')
+print("hello + world") #same output as line 5
+print('hello '"+" ' world')
+a = input("Enter your name: ")
+print("Hello",a)
+print("Hello how are you "+input("Enter your name: "))
+a = "Hello World!"
+
+print(a.upper())  # "HELLO WORLD!"
+print(a.lower())  # "hello world!"
+print(a.title())  # "Hello World!"
+print(a.capitalize())  # "Hello world!"
+print(a.swapcase())  # "hELLO wORLD!"
+print(a.casefold())  # "hello world!"
+print(a.isupper())  # False
+print(a.islower())  # False
+print(a.__len__())  # 12
+print(len(a))  # 12
+
+
+if(print(a)):
+    print("aerwgsawesrhyet")
+
+
--- Python/hello.py
+++
@@ -1,4 +0,0 @@
-print("Hello, World!")
-a = 213
-b = 23
-print(a+b)

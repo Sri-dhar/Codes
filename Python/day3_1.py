fc4ea8f9f01e0395bbee7644806929e2066cd522
sridhartuli <sridhartuli@gmail.com>
2023-11-30 11:22:04 +0530

    ww

--- Python/day3_1.py
+++ Python/day3_1.py
@@ -1,8 +1,35 @@
 a = int(input("Enter a number: "))
 #a = int(a)
-if(a>324):  
+if(a>=324):  
     print("a is greater than 324")
 elif(a<22):
     print("a is less than 22")
 else: 
     print("a is not greater than 324")
+
+
+#checking leap year
+year = int(input("Enter a year: "))
+if(year%400 == 0):
+    print("Leap year")
+elif(year%4 == 0 and year%100 != 0):
+    print("Leap year")
+else:
+    print("Not a leap year")
+
+print("LKJHFODS".lower())
+print("LKJHFODS".count("H"))
+
+
+print('''
+    =/\                 /\=
+    / \'._   (\_/)   _.'/ \     
+   / .''._'--(o.o)--'_.''. \    
+  /.' _/ |`'=/ " \='`| \_ `.\   
+ /` .' `\;-,'\___/',-;/` '. '\  
+/.-' jgs   `\(-V-)/`       `-.\ 
+             "   "              
+      ''')
+
+#print('''\n''') triple ' shows multiline string
+

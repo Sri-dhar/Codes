e38603638e30f50d21ffc8672a481cba7148ddd2
sridhartuli <sridhartuli@gmail.com>
2023-12-04 17:07:07 +0530

    modified:   Python/day16_OOPS.py

---
+++ Python/day16_OOPS.py
@@ -0,0 +1,22 @@
+#Classes and their attributes
+
+# class Car:
+#     def __init__(self, speed, color, fuel):
+#         self.speed = speed
+#         self.color = color
+
+
+import turtle
+
+timmy = turtle.Turtle()
+
+# Move the turtle forward by 100 units
+timmy.forward(100)
+
+# Turn the turtle right by 90 degrees
+timmy.right(90)
+            
+# Move the turtle forward by 100 units
+timmy.forward(100)
+
+turtle.done()

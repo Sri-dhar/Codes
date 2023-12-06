b8be922f507142ca447768baca8be2e4fa728c14
sridhartuli <sridhartuli@gmail.com>
2023-12-06 21:10:51 +0530

    template made

---
+++ cp.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MOD 1e9+7
+#define ll long long
+#define initQ(type, name) priority_queue<type> name
+#define popQ(name) if (!name.empty()) name.pop()
+#define pushQ(name, value) name.push(value)
+#define topQ(name) (!name.empty() ? name.top() : -1)
+#define emptyQ(name) name.empty()
+#define sizeQ(name) name.size()
+#define clearQ(name) while (!name.empty()) name.pop()
+#define initV(type, name, size) vector<type> name(size)
+
+
+void solve()
+{
+
+}
+
+int main() {
+    IOS
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    solve();
+	}
+	return 0;
+}

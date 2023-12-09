b6fdc345242af297854eb355c83cd5edb10bc8e1
sridhartuli <sridhartuli@gmail.com>
2023-12-09 22:48:06 +0530

    new file:   Python/day20/__pycache__/snake.cpython-311.pyc new file:   Python/day20/day20.py new file:   Python/day20/main.py modified:   Python/day20/snake.py new file:   Python/day20/tempCodeRunnerFile.py

---
+++ Python/day20/main.py
@@ -0,0 +1,33 @@
+import random
+import time
+from turtle import Turtle, Screen
+from snake import Snake  
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+# Initial positions of the snake
+Turtle.STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+Turtle.MOVE_DISTANCE = 20
+Turtle.RIGHT = 0
+Turtle.UP = 90
+Turtle.LEFT = 180
+Turtle.DOWN = 270
+
+snake = Snake()  
+screen.update()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()  
+    # snake.turn_right(Turtle.RIGHT)
---
+++ Python/day20/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+
+    def create_snake(self):
+        self.flag = 1
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)  
+            self.segments.append(new_segment)
+
+    def move(self):
+        for segNum in range(len(self.segments) - 1, 0, -1):
+            self.segments[segNum].goto(self.segments[segNum - 1].xcor(), self.segments[segNum - 1].ycor())
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(270)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(0)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(180)

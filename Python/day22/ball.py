9069a0cda6f93bb8aa0663e5b9c79308c0ccab92
sridhartuli <sridhartuli@gmail.com>
2023-12-11 07:52:10 +0530

    some chanegs in pong game (colors)

--- Python/day22/ball.py
+++ Python/day22/ball.py
@@ -4,7 +4,7 @@ class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
-        self.color("white")
+        self.color("bisque")
         self.penup()
         self.x_move = 10
         self.y_move = 10

eb6222c7dd96c3bfa86f0411589920e1c9806f87
sridhartuli <sridhartuli@gmail.com>
2024-02-16 10:56:41 +0530

    Lab 5 code updated

--- AI Lab/Lab5/q1.py
+++ AI Lab/Lab5/q1.py
@@ -2,7 +2,6 @@ import random
 import copy
 import heapq
 
-
 goal_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 possible_input = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 state_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -19,7 +18,6 @@ with open("state_matrix.txt", "w") as file:
         for j in range(3):
             file.write(str(state_matrix[i][j]) + " ")
         file.write("\n")
-# state_matrix = [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
 
 def printState(matrix):
     print("+---+---+---+")
@@ -42,7 +40,7 @@ def is_solvable(puzzle):
 
     return inversions % 2 == 0
 
-if(is_solvable(state_matrix)):
+if is_solvable(state_matrix):
     print("The puzzle is solvable")
 else:
     print("The puzzle is not solvable")
@@ -104,252 +102,14 @@ def moveUp(matrix):
 
 no_of_states_checked = 0
 
-# BFS
-# def BFS(matrix):
-#     queue = []
-#     queue.append(matrix)
-#     while queue:
-#         current = queue.pop(0)
-#         no_of_states_checked += 1
-#         if goalTest(current):
-#             print("Goal State Found")
-#             printState(current)
-#             exit()
-#             return True
-#         else:
-#             up_result = moveUp(current)
-#             if up_result:
-#                 queue.append(up_result)
-#                 printState(up_result)
-#             down_result = moveDown(current)
-#             if down_result:
-#                 queue.append(down_result)
-#                 printState(down_result)
-#             left_result = moveLeft(current)
-#             if left_result:
-#                 queue.append(left_result)
-#                 printState(left_result)
-#             right_result = moveRight(current)
-#             if right_result:
-#                 queue.append(right_result)
-#                 printState(right_result)
-#     return False
-
-# BFS with no. of states checked
-# def BFS(matrix):
-#     global no_of_states_checked  
-#     queue = []
-#     queue.append(matrix)
-#     while queue:
-#         current = queue.pop(0)
-#         no_of_states_checked += 1
-#         if goalTest(current):
-#             print("Goal State Found")
-#             printState(current)
-#             with open('state_matrix.txt', 'a') as f:
-#                 f.write(f'Number of states checked: {no_of_states_checked}\n')
-#             exit()
-#             return True
-#         else:
-#             up_result = moveUp(current)
-#             if up_result:
-#                 queue.append(up_result)
-#                 printState(up_result)
-#             down_result = moveDown(current)
-#             if down_result:
-#                 queue.append(down_result)
-#                 printState(down_result)
-#             left_result = moveLeft(current)
-#             if left_result:
-#                 queue.append(left_result)
-#                 printState(left_result)
-#             right_result = moveRight(current)
-#             if right_result:
-#                 queue.append(right_result)
-#                 printState(right_result)
-#     return False
-
-# BFS with no. of states checked and visited set
-# def BFS(matrix):
-#     global no_of_states_checked  
-#     queue = []
-#     visited = set()  
-#     queue.append(matrix)
-#     visited.add(str(matrix))  
-#     while queue:
-#         current = queue.pop(0)
-#         no_of_states_checked += 1
-#         if goalTest(current):
-#             print("Goal State Found")
-#             printState(current)
-#             with open('state_matrix.txt', 'a') as f:
-#                 f.write(f'Number of states checked using BFS: {no_of_states_checked}\n')
-#             # exit()
-#             return True
-#         else:
-#             up_result = moveUp(current)
-#             if up_result and str(up_result) not in visited:  
-#                 queue.append(up_result)
-#                 visited.add(str(up_result))  
-#                 printState(up_result)
-#             down_result = moveDown(current)
-#             if down_result and str(down_result) not in visited:  
-#                 queue.append(down_result)
-#                 visited.add(str(down_result))  
-#                 printState(down_result)
-#             left_result = moveLeft(current)
-#             if left_result and str(left_result) not in visited:  
-#                 queue.append(left_result)
-#                 visited.add(str(left_result))  
-#                 printState(left_result)
-#             right_result = moveRight(current)
-#             if right_result and str(right_result) not in visited:  
-#                 queue.append(right_result)
-#                 visited.add(str(right_result))
-#                 printState(right_result)
-#     return False
-
-# print("BFS: ", BFS(state_matrix))
-
-# state_matrix = state_matrix_copy
-
-# def DFS(matrix):
-#     global no_of_states_checked
-#     stack = []
-#     visited = set()
-#     stack.append(matrix)
-#     visited.add(str(matrix))
-#     while stack:
-#         current = stack.pop()
-#         no_of_states_checked += 1
-#         if goalTest(current):
-#             print("Goal State Found")
-#             printState(current)
-#             with open('state_matrix.txt', 'a') as f:
-#                 f.write(f'Number of states checked using DFS: {no_of_states_checked}\n')
-#             # exit()
-#             return True
-#         else:
-#             up_result = moveUp(current)
-#             if up_result and str(up_result) not in visited:
-#                 stack.append(up_result)
-#                 visited.add(str(up_result))
-#                 printState(up_result)
-#             down_result = moveDown(current)
-#             if down_result and str(down_result) not in visited:
-#                 stack.append(down_result)
-#                 visited.add(str(down_result))
-#                 printState(down_result)
-#             left_result = moveLeft(current)
-#             if left_result and str(left_result) not in visited:
-#                 stack.append(left_result)
-#                 visited.add(str(left_result))
-#                 printState(left_result)
-#             right_result = moveRight(current)
-#             if right_result and str(right_result) not in visited:
-#                 stack.append(right_result)
-#                 visited.add(str(right_result))
-#                 printState(right_result)
-#     return False
-
-# print("DFS: ", DFS(state_matrix))
-
-# state_matrix = state_matrix_copy
-
-# def iterative_deepening(matrix):
-#     global no_of_states_checked
-#     for i in range(100):
-#         stack = []
-#         visited = set()
-#         stack.append(matrix)
-#         visited.add(str(matrix))
-#         while stack:
-#             current = stack.pop()
-#             no_of_states_checked += 1
-#             if goalTest(current):
-#                 print("Goal State Found")
-#                 printState(current)
-#                 with open('state_matrix.txt', 'a') as f:
-#                     f.write(f'Number of states checked using Iterative Deepening: {no_of_states_checked}\n')
-#                 # exit()
-#                 return True
-#             else:
-#                 up_result = moveUp(current)
-#                 if up_result and str(up_result) not in visited:
-#                     stack.append(up_result)
-#                     visited.add(str(up_result))
-#                     printState(up_result)
-#                 down_result = moveDown(current)
-#                 if down_result and str(down_result) not in visited:
-#                     stack.append(down_result)
-#                     visited.add(str(down_result))
-#                     printState(down_result)
-#                 left_result = moveLeft(current)
-#                 if left_result and str(left_result) not in visited:
-#                     stack.append(left_result)
-#                     visited.add(str(left_result))
-#                     printState(left_result)
-#                 right_result = moveRight(current)
-#                 if right_result and str(right_result) not in visited:
-#                     stack.append(right_result)
-#                     visited.add(str(right_result))
-#                     printState(right_result)
-#     return False
-
-# print("Iterative Deepening: ", iterative_deepening(state_matrix))
-
-# state_matrix = state_matrix_copy
-# def uniform_cost_search(matrix):
-#     global no_of_states_checked
-#     queue = []
-#     visited = set()
-#     queue.append(matrix)
-#     visited.add(str(matrix))
-#     while queue:
-#         current = queue.pop(0)
-#         no_of_states_checked += 1
-#         if goalTest(current):
-#             print("Goal State Found")
-#             printState(current)
-#             with open('state_matrix.txt', 'a') as f:
-#                 f.write(f'Number of states checked using Uniform Cost Search: {no_of_states_checked}\n')
-#             # exit()
-#             return True
-#         else:
-#             up_result = moveUp(current)
-#             if up_result and str(up_result) not in visited:
-#                 queue.append(up_result)
-#                 visited.add(str(up_result))
-#                 printState(up_result)
-#             down_result = moveDown(current)
-#             if down_result and str(down_result) not in visited:
-#                 queue.append(down_result)
-#                 visited.add(str(down_result))
-#                 printState(down_result)
-#             left_result = moveLeft(current)
-#             if left_result and str(left_result) not in visited:
-#                 queue.append(left_result)
-#                 visited.add(str(left_result))
-#                 printState(left_result)
-#             right_result = moveRight(current)
-#             if right_result and str(right_result) not in visited:
-#                 queue.append(right_result)
-#                 visited.add(str(right_result))
-#                 printState(right_result)
-#     return False
-
-# print("Uniform Cost Search: ", uniform_cost_search(state_matrix))
-
-state_matrix = state_matrix_copy
-
 def heuristic(matrix, goal_matrix):
     distance = 0
     for i in range(3):
         for j in range(3):
             if matrix[i][j] != 0:
-                x, y = divmod(matrix[i][j]-1, 3)
+                x, y = divmod(matrix[i][j] - 1, 3)
                 distance += abs(x - i) + abs(y - j)
-    return distance
+    return distance if distance is not None else float('inf')
 
 def greedy_bfs(matrix):
     global no_of_states_checked
@@ -372,18 +132,17 @@ def greedy_bfs(matrix):
                 if result and str(result) not in visited:
                     heapq.heappush(queue, (heuristic(result, goal_matrix), result))
                     visited.add(str(result))
-                    printState(result)
     return False
 
 print("Greedy BFS: ", greedy_bfs(state_matrix))
 
-state_matrix = state_matrix_copy
+no_of_states_checked = 0
 
 def a_star(matrix):
     global no_of_states_checked
     queue = []
     visited = set()
-    heapq.heappush(queue, (heuristic(matrix, goal_matrix), matrix))
+    heapq.heappush(queue, (heuristic(matrix, goal_matrix) + no_of_states_checked, matrix))
     visited.add(str(matrix))
     while queue:
         _, current = heapq.heappop(queue)
@@ -400,7 +159,6 @@ def a_star(matrix):
                 if result and str(result) not in visited:
                     heapq.heappush(queue, (heuristic(result, goal_matrix) + no_of_states_checked, result))
                     visited.add(str(result))
-                    printState(result)
     return False
 
 print("A* Search: ", a_star(state_matrix))

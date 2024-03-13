08848dbc851af7d4d9200b38815c94c0cf73abe5
sridhartuli <sridhartuli@gmail.com>
2024-03-13 18:24:17 +0530

    bash history fixed

--- OS/PracAssignment4/q1.c
+++ OS/PracAssignment4/q1.c
@@ -9,14 +9,22 @@ int count = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
+void print_boxed_message(char* message) {
+    printf("+----------------------------------+\n");
+    printf("| %s\n", message);
+    printf("+----------------------------------+\n");
+}
+
 void* producer(void* arg) {
     while(1) {
         pthread_mutex_lock(&mutex);
         if(count == BUFFER_SIZE) {
             pthread_cond_wait(&cond, &mutex);
         }
+        char message[50];
+        sprintf(message, "Produced: %d, Buffer Size: %d", buffer[count], count);
         buffer[count++] = rand() % 100;
-        printf("Produced: %d\n", buffer[count - 1]);
+        print_boxed_message(message);
         pthread_cond_signal(&cond);
         pthread_mutex_unlock(&mutex);
         for(int i=0;i<1e8;i++);
@@ -29,13 +37,15 @@ void* consumer(void* arg) {
         if(count == 0) {
             pthread_cond_wait(&cond, &mutex);
         }
+        char message[50];
+        sprintf(message, "Consumed: %d, Buffer Size: %d", buffer[count-1], count);
         printf("Consumed: %d\n", buffer[--count]);
+        print_boxed_message(message);
         pthread_cond_signal(&cond);
         pthread_mutex_unlock(&mutex);
         for(int i=0;i<1e9;i++);
     }
 }
-
 int main() {
     pthread_t prod, cons;
     pthread_create(&prod, NULL, producer, NULL);

f3ef0b3b16f1f9077b017f4165ee8615e40f030b
sri-dhar <sridhartuli@gmail.com>
2024-08-16 11:49:50 +0530

    Network Lab 3

--- CF/t.cpp
+++ CF/t.cpp
@@ -1,40 +1,57 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std;    
 
 class Solution {
 public:
-    int res = INT_MAX;
-    int tempfunc(string a, string b) {
-        int n = a.size();
-        int count = 0;
-        for (int i = 0; i < n; i++)
-            a[i] == b[i] ? count++ : 0;
-        return count;
-    }
-
-    void bfs(vector<string>& words, string start, string end, int count) {
-        if(start == end)res = min(res, count);
-        for (int i=0; i<words.size(); i++) {
-            string x = words[i];
-            if (tempfunc(x, start) == 1) {
-                string t = words[i];
-                words[i] = "";
-                bfs(words, t, end, count+1);
-                words[i] = t;
+    int minDays(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+        map<pair<int,int>,int> onesInGrid;
+        bool foundMoreThanOne = false;
+        int componentCount = 0;
+        
+        for(int i = 0; i < m && !foundMoreThanOne; ++i) {
+            for(int j = 0; j < n && !foundMoreThanOne; ++j) {
+                if(grid[i][j]) {
+                    ++onesInGrid[{i,j}];
+                }
+                if(grid[i][j] && !visited[i][j]) {
+                    if (componentCount > 0) {
+                        foundMoreThanOne = true;
+                    } else {
+                        dfs(grid, visited, i, j);
+                        ++componentCount;
+                    }
+                }
             }
         }
+        
+        return foundMoreThanOne ? 2 : componentCount;
     }
 
-    int ladderLength(string beginWord, string endWord, vector<string>& words) {
-        string start = beginWord, end = endWord;
-        bfs(words, start, end, 0);
-        return res == INT_MIN ? 0 : res+1;
+    void dfs(const vector<vector<int>>& grid, vector<vector<int>>& visited, int x, int y) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<int> directions = {-1, 0, 1, 0, 0, -1, 0, 1};
+        
+        visited[x][y] = 1;
+        
+        for(int d = 0; d < 4; ++d) {
+            int newX = x + directions[d*2];
+            int newY = y + directions[d*2 + 1];
+            
+            if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] && !visited[newX][newY]) {
+                dfs(grid, visited, newX, newY);
+            }
+        }
     }
 };
 
 int main() {
     Solution s;
-    vector<string> words = {"hot","dot","dog","lot","log","cog"};
-    cout << s.ladderLength("hit", "cog", words) << endl;
+    vector<vector<int>> grid = {{1,1},{1,1}};
+    cout << s.minDays(grid) << endl;
     return 0;
 }
